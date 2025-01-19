53ce502f4af63f9ca4ce35760826b63ffc770a84
hossain-shahriar <mhossai6@ualberta.ca>
2025-01-19 16:28:43 -0700

    fix: remove world map link in projects section

--- app/Components/Projects.tsx
+++ app/Components/Projects.tsx
@@ -121,17 +121,7 @@ const Projects = () => {
       {projectsData.map((project, index) => (
         <Project key={index} index={index} {...project} />
       ))}
-      {/* Add a link to the extra HTML file */}
-      <div className="text-center mt-10">
-        <a
-          href="/event_map.html"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-blue-600 hover:underline text-lg font-bold"
-        >
-          View More Projects →
-        </a>
-      </div>
+      
     </section>
   );
 }
